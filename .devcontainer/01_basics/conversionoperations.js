07db94c5109ee9eabf06664e80dad97bf9ca2548
saeed <111153188+muhammad-saeed600@users.noreply.github.com>
2025-02-12 17:39:13 +0000

    datatypes conversion

---
+++ .devcontainer/01_basics/conversionoperations.js
@@ -0,0 +1,24 @@
+let age= "thirty"
+
+
+//conver string into number
+let isNumberValue= Number(age)
+console.log(isNumberValue);
+
+//convert string into boolean
+let isStringValue = Boolean(age)
+console.log(isStringValue);
+
+
+let isLoggedIn = false
+
+//convert boolean into number
+let isBooleanValue= Number(isLoggedIn)
+console.log(isBooleanValue);
+console.log(typeof isBooleanValue);
+
+let score= 30
+
+//convert number into String
+let inString= String(score)
+console.log(inString);
