31e8f609b81d89b82e3f93f8b0fdc06b4952ef8d
saeed <111153188+muhammad-saeed600@users.noreply.github.com>
2025-02-12 12:34:17 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,20 @@
+let sname= "saeed" //string
+var age= 23//number
+let state;//undefined
+let isLoggedIN= false //boolean
+let id= null // standalone value (no value)
+
+
+//find type of dataypes by two methods
+console.log(typeof sname);
+console.log(typeof(sname));
+
+//data types------
+//string= ""
+//BigInt
+//boolean= true/false
+//number= 2 ki power 53
+//null =standalone value(no value)
+//undefine like => let id;
+//symbol =unique
+//object
